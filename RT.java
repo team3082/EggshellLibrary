e37a2a09b9243235ff2cad80d2a35e74580994be
Forrest <44622688+ForrestZaneJohnson@users.noreply.github.com>
2020-03-04 11:14:30 -0600

    RT.java updated with changes made in 2020 Infinite Recharge

--- RT.java
+++ RT.java
@@ -1,11 +1,44 @@
 package frc.lib;
 
-public class RT { 
+public class RT {
 
-    public static double m_time; 
-    
-    public static double rawTick () {
-        return m_time / .02; 
+    public static double m_time;
+
+    public static double getTick () {
+
+	  return (int) m_time / .02;
+
+  }
+
+  public static double getTick (double time) {
+
+    return (int) time / .02;
+
+  }
+
+	public static boolean isTime (double time) {
+
+		return getTick(time) == getTick(m_time);
+
+    }
+
+    public static boolean isTime (double time, double range) {
+
+		return time - range <= m_time && time + range >= m_time;
+
+	}
+
+
+    public static boolean isAfter (double time) {
+
+		return time > m_time;
+
+    }
+
+    public static boolean isBefore (double time) {
+
+		return time < m_time;
+
     }
 
 }
