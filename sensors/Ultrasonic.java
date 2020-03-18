48a1ea8e1da217e56b522ebbb5cedfaba871ec83
Forrest Johnson <Forrest.z.johnson@gmail.com>
2020-03-18 13:34:33 -0500

    Mod: ultrasonic port made final

--- sensors/Ultrasonic.java
+++ sensors/Ultrasonic.java
@@ -2,18 +2,23 @@ package frc.lib.sensors;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
-//https://www.maxbotix.com/documents/HRLV-MaxSonar-EZ_Datasheet.pdf
+// https://www.maxbotix.com/documents/HRLV-MaxSonar-EZ_Datasheet.pdf
+
 public class Ultrasonic {
 
-    private AnalogInput m_ultrasonic;
+    private final AnalogInput m_ultrasonic;
     private final double kCentimetersPerVolt = 102.3541453428864;
 
     public Ultrasonic(int analogPort) {
+
         m_ultrasonic = new AnalogInput(analogPort);
+
     }
 
     public double getDistance () {
+
         return m_ultrasonic.getVoltage() * kCentimetersPerVolt;
+
     }
 
 }
