26f990c4656a746f6b7e09cdadbfaa6d2a0b83a7
Forrest Johnson <Forrest.z.johnson@gmail.com>
2020-03-18 13:50:36 -0500

    Mod: null check added for Strings in Log.java

--- Log.java
+++ Log.java
@@ -5,33 +5,75 @@ public class Log {
     public static boolean m_enabled = false;
 
     public static void println(Object str) {
+		
         if (m_enabled) {
+			
             if (str == null) {
+				
                 System.out.println("null");
+				
             } else {
+				
                 System.out.println(str.toString());
+				
             }
+			
         }
+		
     }
 
     public static void println(String str) {
-        if (m_enabled)
-            System.out.println(str);
-    }
-
-    public static void print(Object str) {
+		
         if (m_enabled) {
+			
+			if (str == null) {
+				
+				System.out.println("null"); 
+				
+			} else {
+			
+				System.out.println(str);
+			
+			}
+			
+		}
+			
+    }
+	
+    public static void print(Object str) {
+		
+        if (m_enabled) {
+			
             if (str == null) {
+				
                 System.out.print("null");
+				
             } else {
+				
                 System.out.print(str.toString());
+				
             }
+			
         }
+		
     }
 
     public static void print(String str) {
-        if (m_enabled)
-            System.out.print(str);
+		
+        if (m_enabled) {
+			
+			if (str == null) {
+				
+				System.out.print("null"); 
+				
+			} else {
+			
+				System.out.print(str);
+			
+			}
+			
+		}
+		
     }
 
 }
